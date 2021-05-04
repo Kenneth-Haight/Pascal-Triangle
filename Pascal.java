28acbd033a8e1941a70802387ca17cfc80d445ab
Kenneth-Haight <82239058+Kenneth-Haight@users.noreply.github.com>
2021-05-04 18:15:42 -0400

    Update Pascal.java

--- Pascal.java
+++ Pascal.java
@@ -1,9 +1,6 @@
 //********************************************************************
 //  Pascal.java       Author: Kenneth Haight
 //
-//  Creates Pascal's Triangle using recursion reliably up to 15 rows
-//  The program is recursive on line 38 by calling the method upon itself twice in its return method, which
-//  essentially prints the triangles shape and fills in the numbers according to both the sum of the two recursive calls
 //********************************************************************
 import java.util.Scanner;
 public class Pascal {
