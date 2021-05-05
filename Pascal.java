b19ec002ec89afc3d93e9972fa1ab3fce19e5e5e
Kenneth Haight <82239058+Kenneth-Haight@users.noreply.github.com>
2021-05-04 20:20:00 -0400

    Update Pascal.java

--- Pascal.java
+++ Pascal.java
@@ -20,7 +20,7 @@ public class Pascal {
 	    	}
 	    	p--;
 	    	System.out.print(" ");
-	      for (int j = 0; j <= i; j++) {
+	    for (int j = 0; j <= i; j++) {
 	        System.out.print(triangle(i, j) + "     ");
 
 	      }
@@ -31,7 +31,8 @@ public class Pascal {
 	  public static int triangle(int i, int j) {
 	    if (j == 0 || j == i) {
 	      return 1;
-	    } else {
+	    } 
+	    else {
 	      return triangle(i - 1, j - 1) + triangle(i - 1, j);
 	    }
 	  }
